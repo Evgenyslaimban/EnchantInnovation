6918dc188ebe41de684e7211356c8c86c8056671
Evgenyslaimban <71488387+Evgenyslaimban@users.noreply.github.com>
2025-06-07 00:18:57 +0800

    Add NeoForge event handlers and adjust exp logic

--- common/src/main/java/com/yyz/enchantinnovation/mixin/ItemStackMixin.java
+++ common/src/main/java/com/yyz/enchantinnovation/mixin/ItemStackMixin.java
@@ -10,6 +10,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.ArmorItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -39,6 +40,13 @@ public class ItemStackMixin {
 
     @Inject(method = "hurtAndBreak(ILnet/minecraft/world/entity/LivingEntity;Lnet/minecraft/world/entity/EquipmentSlot;)V", at = @At("RETURN"))
     private void injectDamage(int i, LivingEntity livingEntity, EquipmentSlot equipmentSlot, CallbackInfo ci) {
-        EnchantmentUtils.addExp(stack, i);
+        int exp = i;
+        Item item = stack.getItem();
+
+        if (item instanceof ArmorItem) {
+            exp = i * 5;
+        }
+
+        EnchantmentUtils.addExp(stack, exp);
     }
 }
