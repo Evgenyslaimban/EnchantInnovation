1d14960b8f1f87d6d88e54c7d2043bcac8190f31
Evgenyslaimban <71488387+Evgenyslaimban@users.noreply.github.com>
2025-06-06 21:49:12 +0000

    бла бла?

--- common/src/main/java/com/yyz/enchantinnovation/mixin/ItemStackMixin.java
+++ common/src/main/java/com/yyz/enchantinnovation/mixin/ItemStackMixin.java
@@ -7,7 +7,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -41,7 +41,7 @@ public class ItemStackMixin {
         int exp = i;
         Item item = stack.getItem();
 
-        if (item instanceof ArmorItem) {
+        if (item instanceof ArrowItem) {
             exp = i * 5;
         }
 
